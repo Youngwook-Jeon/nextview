95757c72e4fa048212d6fc1859771d41f5a728a4
Youngwook-Jeon <ela87@naver.com>
2025-02-15 17:45:49 +0900

    Authenticate users by using the Clerk

---
+++ src/app/(home)/protected/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+  return (
+    <div>Only logged in users can see this</div>
+  )
+}
+
+export default Page
--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-
+import { useAuth, useClerk } from "@clerk/nextjs";
 import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
 
 import {
@@ -32,6 +32,9 @@ const items = [
 ];
 
 export const MainSection = () => {
+  const clerk = useClerk();
+  const { isSignedIn } = useAuth();
+
   return (
     <SidebarGroup>
       <SidebarGroupContent>
@@ -42,7 +45,12 @@ export const MainSection = () => {
                 tooltip={item.title}
                 asChild
                 isActive={false}
-                onClick={() => {}}
+                onClick={(e) => {
+                  if (!isSignedIn && item.auth) {
+                    e.preventDefault();
+                    return clerk.openSignIn();
+                  }
+                }}
               >
                 <Link href={item.url} className="flex items-center gap-4">
                   <item.icon />
